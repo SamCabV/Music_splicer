5c25793d10f8b9e6f69c8f4be19ac49f3154150d
SamCabV <scabreravalencia@olin.edu>
2020-12-12 20:30:28 -0500

    full_pipline_1

--- findGlobalDifference.m
+++ findGlobalDifference.m
@@ -1,7 +1,7 @@
-function [global_min, I_global] = findGlobalDifference(user_song_spect, spect_lib)
-global_min_list = zeros(2,length(song_lib));
-for i = 1:length(song_lib)
-    [global_min_list(1,i),global_min_list(2,i)] = findMinDifference(user_song_spect, spect_lib(i));
+function [global_min, I_global] = findGlobalDifference(user_song_spect, spect_lib, t_song_init)
+global_min_list = zeros(2,length(spect_lib));
+for i = 1:length(spect_lib)
+    [global_min_list(1,i),global_min_list(2,i),~,~] = findMinDifference(user_song_spect, spect_lib{i},t_song_init);
 end
-[global_min, I_global] = min(global_min_list);
+[global_min,I_global] = min(global_min_list(1,:));
 end
--- findMinDifference.m
+++ findMinDifference.m
@@ -1,5 +1,8 @@
-function [best_min, I_best_min] = findMinDifference(s1, s2)
-stop_s1 = round(map(15, 0, 30, 1, length(s1)));
+function [best_min, I_best_min,start_s2, stop_s1] = findMinDifference(s1, s2, t_song)
+
+stop_s1 = map(t_song, 0, 30, 1, length(s1));
+stop_s1 = round(stop_s1);
+
 min_list = zeros((length(s1)-stop_s1),3);
 min_list = transpose(min_list);
 for i = 1:length(min_list)
@@ -11,4 +14,7 @@ for i = 1:length(min_list)
 end
 
 [best_min,I_best_min] = min(min_list(1,:));
+start_s2 = min_list(2,I_best_min);
+stop_s1 = min_list(3, I_best_min);
+
 end
